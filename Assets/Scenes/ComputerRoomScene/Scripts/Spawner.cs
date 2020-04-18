e82fe8b19120b783210637b5ea1a58cacf94675e
2door <tudor.cismarescu@gmail.com>
2020-04-18 14:35:19 -0400

    feat: adds enemy spawning

---
+++ Assets/Scenes/ComputerRoomScene/Scripts/Spawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour{
+
+    public float top;
+    public float bot;
+    public float right;
+    public float left;
+    public float spawnRate;
+    public GameObject enemySpawnPrefab;
+    public GameObject enemyPrefab;
+
+    private GameObject[] spawners;
+    private bool spawnLock = true;
+
+    // Start is called before the first frame update
+    void Start() {
+        spawners = new GameObject[4];
+        spawners[0] = Instantiate(enemySpawnPrefab, new Vector2(left, top), Quaternion.identity);
+        spawners[1] = Instantiate(enemySpawnPrefab, new Vector2(right, top), Quaternion.identity);
+        spawners[2] = Instantiate(enemySpawnPrefab, new Vector2(left, bot), Quaternion.identity);
+        spawners[3] = Instantiate(enemySpawnPrefab, new Vector2(right, bot), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (spawnLock) {
+            StartCoroutine(Spawn());
+        }
+    }
+
+    private IEnumerator Spawn() {
+        spawnLock = false;
+        foreach (GameObject spawnerInstance in spawners) {
+            Instantiate(enemyPrefab, spawnerInstance.transform.position, Quaternion.identity);
+        }
+        yield return new WaitForSeconds(spawnRate);
+        spawnLock = true;
+    }
+}
